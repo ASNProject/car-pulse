dc9780018abac6153b5face0b7c51bfb87165795
ASNProject <asnproject02@gmail.com>
2026-01-23 10:14:15 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CarPulseController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HistoryController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+
 Route::apiResource('/carpulses', CarPulseController::class);
+Route::delete('/history/truncate', [HistoryController::class, 'truncate']);
